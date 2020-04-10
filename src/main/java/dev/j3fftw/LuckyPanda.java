ead2814e39ebaa1d1f7155ab1c4bfae815ccc212
Panda.com <J3fftw@gmail.com>
2020-04-11 01:20:57 +0200

    Initial Commit

---
+++ src/main/java/dev/j3fftw/LuckyPanda.java
@@ -0,0 +1,32 @@
+package dev.j3fftw;
+
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LuckyPanda extends JavaPlugin implements SlimefunAddon {
+
+    private static LuckyPanda instance;
+
+    public static LuckyPanda getInstance() {
+        return instance;
+    }
+
+    public void onEnable() {
+        instance = this;
+
+        getServer().getPluginManager().registerEvents(new Events(), this);
+
+    }
+
+    public void onDisable() {
+        instance = null;
+    }
+
+    public JavaPlugin getJavaPlugin() {
+        return this;
+    }
+
+    public String getBugTrackerURL() {
+        return "https://github.com/j3fftw/LuckyPanda";
+    }
+}
